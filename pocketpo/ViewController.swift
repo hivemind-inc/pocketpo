e5740e021e9adb6dc8485787c468252c28d4ae26
Devin Brown <devin.g.brown@gmail.com>
2014-11-19 16:23:37 -0700

    gitignore + single page app

---
+++ pocketpo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pocketpo
+//
+//  Created by OBFK on 11/19/14.
+//  Copyright (c) 2014 com.obesity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

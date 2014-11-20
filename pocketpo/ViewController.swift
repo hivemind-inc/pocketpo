b279fc633cb8d9e694cc7217296a1618398243bb
Devin Brown <devin.g.brown@gmail.com>
2014-11-19 17:43:46 -0700

    add get Category + Item NSURL / download calls

--- pocketpo/ViewController.swift
+++ pocketpo/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let baseURL = NSURL(string: "http://pocketpo.herokuapp.com/api/v1/")
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        getCategoryData()
+        getItemData()
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,41 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func getCategoryData() -> Void {
+
+        let categoryURL = NSURL(string: "categories", relativeToURL: baseURL)
+
+        let sharedSession = NSURLSession.sharedSession()
+
+        let downloadTask: NSURLSessionDownloadTask = sharedSession.downloadTaskWithURL(categoryURL!, completionHandler: { (
+            location: NSURL!,
+            response: NSURLResponse!,
+            error: NSError!) -> Void in
+
+            println(response)
+        })
+
+        downloadTask.resume()
+
+    }
+
+    func getItemData() -> Void {
+
+        let itemURL = NSURL(string: "items?tag=cats", relativeToURL: baseURL)
+
+        let sharedSession = NSURLSession.sharedSession()
+
+        let downloadTask: NSURLSessionDownloadTask = sharedSession.downloadTaskWithURL(itemURL!, completionHandler: { (
+            location: NSURL!,
+            response: NSURLResponse!,
+            error: NSError!) -> Void in
+
+            println(response)
+        })
+
+        downloadTask.resume()
+
+    }
 
 }
 

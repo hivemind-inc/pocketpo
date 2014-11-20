563673e655d1c377134f7c0816fc43782b957557
Devin Brown <devin.g.brown@gmail.com>
2014-11-19 17:18:31 -0700

    add basic / default category struct

---
+++ pocketpo/Category.swift
@@ -0,0 +1,20 @@
+//
+//  Categories.swift
+//  pocketpo
+//
+//  Created by OBFK on 11/19/14.
+//  Copyright (c) 2014 com.obesity. All rights reserved.
+//
+
+import Foundation
+
+struct Category {
+    
+    var id: Int
+    var name: String
+    
+    init() {
+        id = 0
+        name = "default"
+    }
+}
